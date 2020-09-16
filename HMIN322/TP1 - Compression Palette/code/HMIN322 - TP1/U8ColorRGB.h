25f9531ebe185f890956c055ce190174b39aaf12
Eikins <eric.kinnser@gmail.com>
2020-09-16 14:39:20 +0200

    HMIN322 - Finish TP1

--- HMIN322/TP1 - Compression Palette/code/HMIN322 - TP1/U8ColorRGB.h
+++ HMIN322/TP1 - Compression Palette/code/HMIN322 - TP1/U8ColorRGB.h
@@ -13,7 +13,7 @@ public:
 	U8ColorRGB() { r = 0; g = 0; b = 0; }
 	U8ColorRGB(int red, int green, int blue) { r = red; g = green; b = blue; }
 	inline int magSq() { return r * r + g * g + b * b; }
-	inline int distSq(U8ColorRGB target) { return (r - target.r) * (r - target.r) + (g - target.g) * (g - target.g) + (b - target.b) * (b - target.b); }
+	inline int distSq(const U8ColorRGB &target) const { return (r - target.r) * (r - target.r) + (g - target.g) * (g - target.g) + (b - target.b) * (b - target.b); }
 
-	bool operator==(U8ColorRGB other) { return r == other.r && g == other.g && b == other.b; }
+	bool operator==(const U8ColorRGB &other) const { return r == other.r && g == other.g && b == other.b; }
 };
